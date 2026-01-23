7adf8beba08d380110129c10ee4427bc7402f98e
ketdemur-design <ketdemur@gmail.com>
2026-01-23 23:30:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 import os
+from flask import Flask, request
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-# Берём токен из Railway Variables
-TOKEN = os.environ.get("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 
-if not TOKEN:
-    raise ValueError("BOT_TOKEN is not set")
+app = Flask(__name__)
+
+telegram_app = Application.builder().token(TOKEN).build()
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот работает ✅")
 
-def main():
-    app = Application.builder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.run_polling()
 
-if __name__ == "__main__":
-    main()
+telegram_app.add_handler(CommandHandler("start", start))
+
+
+@app.route("/webhook", methods=["POST"])
+async def webhook():
+    update = Update.de_json(request.get_json(force=True), telegram_app.bot)
+    await telegram_app.process_update(update)
+    return "ok"
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return "Bot is running"
+
 
